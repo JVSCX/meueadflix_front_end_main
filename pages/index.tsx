40afca1143477978065fb52ee03194f78702c956
JVSCX <joaovittorsp@hotmail.com.br>
2023-03-23 21:31:09 -0300

    Configuração do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss";
+
+const HomeNoAuth = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default HomeNoAuth;
