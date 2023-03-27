394da250e13e7a6a257d183896151a6aad99cd3d
JVSCX <joaovittorsp@hotmail.com.br>
2023-03-26 23:59:00 -0300

    Conexão com o Backend

---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,27 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+const SlideComponent = function ({ course }: props) {
+  return (
+    <>
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 4,
+            perMove: 1,
+            pagination: false,
+          }}
+        >
+          {course?.map((course) => (
+            <SplideSlide key={course.id}>
+              <SlideCard course={course} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </>
+  );
+};
+
+export default SlideComponent;
