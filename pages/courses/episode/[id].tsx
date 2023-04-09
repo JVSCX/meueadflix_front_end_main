ddecbd61683cc3aea18557b32ea6a0bcf506d746
JVSCX <joaovittorsp@hotmail.com.br>
2023-04-09 16:46:22 -0300

    Implementando autenticação em todas as paginas

--- pages/courses/episode/[id].tsx
+++ pages/courses/episode/[id].tsx
@@ -22,6 +22,7 @@ const EpisodePlayer = function () {
 
   const [getEpisodeTime, setGetEpisodeTime] = useState(0);
   const [episodeTime, setEpisodeTime] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const playerRef = useRef<ReactPlayer>(null);
 
@@ -84,12 +85,24 @@ const EpisodePlayer = function () {
     getCourse();
   }, [courseId]);
 
+  useEffect(() => {
+    if (!sessionStorage.getItem("meueadflix-token")) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
   if (course?.episodes === undefined) return <PageSpinner />;
 
   if (episodeOrder + 1 < course?.episodes?.length) {
     if (Math.round(episodeTime) === course.episodes[episodeOrder].secondsLong) {
       handleNextEpisode();
     }
+  }
+
+  if (loading) {
+    return <PageSpinner />;
   }
 
   return (
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,30 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "reactstrap";
 import Footer from "../src/components/common/footer";
 import HeaderAuth from "../src/components/common/headerAuth";
+import PageSpinner from "../src/components/common/spinner";
 import PasswordForm from "../src/components/profile/password";
 import UserForm from "../src/components/profile/user";
 import styles from "../styles/profile.module.scss";
 
 const UserInfo = function () {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!sessionStorage.getItem("meueadflix-token")) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) {
+    return <PageSpinner />;
+  }
+
   const [form, setForm] = useState("userForm");
 
   return (
