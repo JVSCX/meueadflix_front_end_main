6de2494f549c297b29f3521dc11250f04b6adf20
JVSCX <joaovittorsp@hotmail.com.br>
2023-04-09 12:31:47 -0300

    back da pagina de curso

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -90,11 +90,64 @@ const courseService = {
 
     return res;
   },
+  like: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("meueadflix-token");
+
+    const res = await api
+      .post(
+        "/likes",
+        { courseId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
+    return res;
+  },
+  removeLike: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("meueadflix-token");
+
+    const res = await api
+      .delete(`/likes/${courseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
+    return res;
+  },
   getSearch: async (name: string) => {
     const token = sessionStorage.getItem("meueadflix-token");
 
     const res = await api
       .get(`/courses/search?name=${name}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("meueadflix-token");
+
+    const res = await api
+      .get(`/courses/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
